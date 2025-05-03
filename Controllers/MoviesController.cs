edb5f62353cffb5f5e13cebe02dc7a3c89125023
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-05-03 17:42:45 +0700

    menambahkan API Movies

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_103022300061.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> _movies = new List<Movie>
+        {
+            new Movie("The Shawshank Redemption", "Frank Darabont", 
+                new List<string>{"Tim Robbins", "Morgan Freeman", "Bob Gunton"}, 
+                "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
+            new Movie("The Godfather", "Francis Ford Coppola", 
+                new List<string>{"Marlon Brando", "Al Pacino", "James Caan"}, 
+                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."),
+            new Movie("The Dark Knight", "Christopher Nolan", 
+                new List<string>{"Christian Bale", "Heath Ledger", "Aaron Eckhart"}, 
+                "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.")
+        };
+
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return (_movies);
+        }
+
+        // GET api/<MoviesController>/
+        [HttpGet("{id}")]
+        public ActionResult<Movie> GetMoviebyId(int id)
+        {
+            if (id < 0 || id >= _movies.Count)
+            {
+                return NotFound("Mahasiswa tidak ditemukan");
+            }
+            return Ok(_movies[id]);
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public ActionResult<Movie> Post([FromBody] Movie movie)
+        {
+            if (movie == null)
+            {
+                return BadRequest("Attribute movie tidak boleh kosong.");
+            }
+            _movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovies), new {id = _movies.Count - 1}, movie);
+        }
+
+        // DELETE api/<MoviesController>/
+        [HttpDelete("{id}")]
+        public ActionResult<Movie> Delete(int id)
+        {
+            if (id < 0 || id >= _movies.Count)
+            {
+                return NotFound("Movie tidak ditemukan");
+            }
+            _movies.RemoveAt(id);
+            return Ok(_movies);
+        }
+    }
+}
--- obj/Debug/net8.0/modul10_103022300061.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul10_103022300061.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul10_103022300061")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde6e196190b9a1ea96bc39f1ed3a9612c5daf2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul10_103022300061")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul10_103022300061")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
